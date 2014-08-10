a70d23894c1f05f25302202d526b3d67d947e0c4
Dima M <message.dimke@gmail.com>
2014-08-10 15:15:31 +0300

    Adding changes to the process-vm module, description added, task refactored to read the task options from the grunt file: fileName and fileContent. Some unnecessary files were removed.

--- tasks/process-vm.js
+++ tasks/process-vm.js
@@ -1,9 +1,22 @@
 'use strict';
-
 module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-exec');
-
-    grunt.registerTask('process-vm-template', '', function (fileName, fileContent) {
-        grunt.task.run('exec:renderVmTemplate:' + fileName + ':' + fileContent);
+    grunt.initConfig({
+        exec: {
+            process: {
+                cmd: function(fileName, fileContent) {
+                    var path = __dirname + "/..";
+                    return 'java -cp ' + path + '/vendor/templator.jar com.wix.Templator "' + fileName + '" "' + fileContent + '"';
+                }
+            }
+        }
     });
+    grunt.registerTask('process-vm', 'Processes VM Files.', function() {
+        var options = this.options({
+            fileName: "",
+            fileContent: ""
+        });
+        grunt.task.run("exec:process:" + options.fileName + ":b" + options.fileContent);
+    });
+    grunt.registerTask('build', ['process-vm']);
 };
