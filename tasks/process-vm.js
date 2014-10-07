197ec9b6d0088bc1bd5904eb04c8c62617869a4e
pavelme <pavelme@wix.com>
2014-10-07 16:17:13 +0300

    extendede grunt task to accept one more parameter

--- tasks/process-vm.js
+++ tasks/process-vm.js
@@ -4,19 +4,20 @@ module.exports = function (grunt) {
 
     grunt.config('exec', {
         process: {
-            cmd: function(view, payload, output) {
+            cmd: function(view, payload, output, mode) {
                 var path = __dirname + "/..";
-                return 'java -jar ' + path + '/vmrenderer/vmrenderer.jar ' + view + ' \'' + decodeURIComponent(payload) + '\' ' + output;
+                return 'java -jar ' + path + '/vmrenderer/vmrenderer.jar ' + view + ' \'' + decodeURIComponent(payload) + '\' ' + output + ' ' + mode;
             }
         }
     });
 
-    grunt.registerTask('process-vm', 'Processes VM Files.', function(view, payload, output) {
+    grunt.registerTask('process-vm', 'Processes VM Files.', function(view, payload, output, mode) {
         var options = this.options({
             view: view || "",
             payload: payload || "",
-            output: output || ""
+            output: output || "",
+            mode: mode || ""
         });
-        grunt.task.run("exec:process:" + options.view + ":" + options.payload + ":" + options.output);
+        grunt.task.run("exec:process:" + options.view + ":" + options.payload + ":" + options.output + ":" + options.mode);
     });
 };
